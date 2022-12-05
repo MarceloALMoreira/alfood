61b34dca453bc1a291afc6e5f6b8d2bf4eb03ec6
marcelo.almeida <90574339+MarceloALMoreira@users.noreply.github.com>
2022-12-05 09:26:12 -0300

    feat api proxima pagina

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,6 +1,6 @@
 import IRestaurante from '../../interfaces/IRestaurante';
 import { IPaginacao } from '../../interfaces/IPaginacao';
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 import axios from 'axios';
@@ -9,26 +9,35 @@ import axios from 'axios';
 // fazendo a implatação das  api
 const ListaRestaurantes = () => {
 
-   const [restaurantes, setResraurantes] = useState<IRestaurante[]>([])
+  const [restaurantes, setResraurantes] = useState<IRestaurante[]>([])
 
-  const [proximaPagina, setProximaPagina] =useState('')
+  const [proximaPagina, setProximaPagina] = useState('')
 
   useEffect(() => {
     //vamos obter os restaurantes passado o link url
     axios.get<IPaginacao<IRestaurante>>('http://127.0.0.1:8000/api/v1/restaurantes/')
-    .then(resposta =>{
-      setResraurantes(resposta.data.results)
-      setProximaPagina(resposta.data.next)
-    })
+      .then(resposta => {
+        setResraurantes(resposta.data.results)
+        setProximaPagina(resposta.data.next)
+      })
       .catch(erro => {
-      console.log(erro)
-    })
+        console.log(erro)
+      })
   }, [])
+
+  const verMais = () => {
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then(resposta => {
+        setResraurantes([...restaurantes, ...resposta.data.results])
+        setProximaPagina(resposta.data.next)
+
+      })
+  }
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-    { proximaPagina &&  <button>Ver mais</button>}
+    {proximaPagina && <button onClick={verMais} className={style.Buttao}>Ver mais</button>}
   </section>)
 }
 
