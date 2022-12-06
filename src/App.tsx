23bcd3267833989ce34f28eb9bc709f08d6d52c7
MarceloALMoreira <marceloaomoreira>
2022-12-05 21:42:03 -0300

    feat: criando nova routa

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import AdmRestaurantes from './paginas/Admin';
 import { FormularioRestaurante } from './paginas/Admin/FormularioRestaurante';
+import PaginaBaseAdmin from './paginas/Admin/PaginaBaseAdmin';
 import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 
@@ -10,9 +11,16 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
-      <Route path="/admin/restaurantes" element={<AdmRestaurantes />} />
-      <Route path="/admin/restaurantes/novo" element={<FormularioRestaurante />} />
-      <Route path="/admin/restaurantes/:id" element={<FormularioRestaurante />} />
+
+      <Route path="/admin"element={<PaginaBaseAdmin/>}>        
+
+      <Route path="restaurantes" element={<AdmRestaurantes />} />
+      <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
+        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
+        
+      </Route>
+
+
     </Routes>
   );
 }
---
+++ src/paginas/Admin/PaginaBaseAdmin.tsx
@@ -0,0 +1,41 @@
+import {  Button, Typography, Box, AppBar, Container, Toolbar, Link, Paper } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+const PaginaBaseAdmin = () =>   {    
+
+    return (
+        <>
+            <AppBar position='static'>
+                <Container maxWidth='xl'>
+                    <Toolbar>
+                        <Typography variant='h6'>
+                            Adminstração
+                        </Typography>
+                        <Box>
+                            <Link component={RouterLink} to='/admin/restaurantes'>
+                                <Button sx={{ my: 2, color: 'white' }}>
+                                    Restaurante
+                                </Button>
+                            </Link>
+                            <Link component={RouterLink} to='/admin/restaurantes/novo'>
+                                <Button sx={{ my: 2, color: 'white' }}>
+                                    Novo Restaurante
+                                </Button>
+                            </Link>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <Box>
+                <Container maxWidth='lg' sx={{ mt: 1 }}>
+                    <Paper sx={{ p: 2 }}>
+                        {/* Conteudo da pagina */}
+                        
+                    </Paper>
+                </Container>
+            </Box>
+        </>
+
+    )
+}
+export default PaginaBaseAdmin
